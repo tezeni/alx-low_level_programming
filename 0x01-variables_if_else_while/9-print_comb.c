d364aea5a68ef600584a2642c3bd7ce8634880dc
tezeni <teztig@gmail.com>
2023-04-21 16:03:50 -0700

    cm

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success):
+**/
+int main(void)
+{
+int i;
+for (i  =       0;    i       <=      9;    i++)
+{
+if (i	=	9)
+{
+putchar(i);
+}
+else
+{
+putchar(i);
+putchar(',';
+putchar(' ');
+}
+}
+return  (0);
+}
