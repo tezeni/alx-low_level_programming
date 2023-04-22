10cea810dd53f459fbb487ba59fe91a1585fbf47
tezeni <teztig@gmail.com>
2023-04-21 19:36:08 -0700

    cm

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,34 +6,36 @@
 **/
 int main(void)
 {
-int i;
-int j;
-int k;
-int l;
-for (i  =       0;      i       <=      9;      i++)
-{
-for (j  =       0;      j       <=      9;      j++)
-{
-for (k  =       0;      k       <=      9;      k++)
-{
-for (l	=	0;	l	<=	9;	l++)
-{
-if (i	==	k	&&	j	==	l)
-continue;
-if (i	==	k	&&	i	>	j)
-continue;
-putchar('0'     +       i);
-putchar('0'     +       j);
-putchar(' ');
-putchar('0'     +       k);
-putchar('0'	+	l);
-if (j   ==      8       &&      k       ==      9)
-continue;
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-return	(0);
+        int ifirst;
+        int i;
+        int jfirst;
+        int j;
+
+        for (ifirst = 48; ifirst < 58; ifirst++)
+        {
+                for (i = 48; i < 58; i++)
+                {
+                        j = i + 1;
+                        jfirst = ifirst;
+                        for (; jfirst < 58; jfirst++)
+                        {
+                                for (; j < 58; j++)
+                                {
+                                        putchar(ifirst);
+                                        putchar(i);
+                                        putchar(' ');
+                                        putchar(jfirst);
+                                        putchar(j);
+                                        if (ifirst != 57  jfirst != 57  i != 56 || j != 57)
+                                        {
+                                                putchar(',');
+                                                putchar(' ');
+                                        }
+                                }
+                                j = 48;
+                        }
+                }
+        }
+        putchar('\n');
+        return (0);
 }
