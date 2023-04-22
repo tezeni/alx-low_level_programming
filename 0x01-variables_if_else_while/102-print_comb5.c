6ede932ee03d84579ba8766f9d53d85b0bf7f239
tezeni <teztig@gmail.com>
2023-04-21 18:50:54 -0700

    cm

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success):
+**/
+int main(void)
+{
+int i;
+int j;
+int k;
+int l
+for (i  =       0;      i       <=      9;      i++)
+{
+for (j  =       0;      j       <=      9;      j++)
+{
+for (k  =       0;      k       <=      9;      k++)
+{
+for (l	=	0;	l	<=	9;	l++)
+{
+if (j   ==      k)
+continue;
+putchar('0'     +       i);
+putchar('0'     +       j);
+putchar('0'     +       k);
+putchar('0'	+	l);
+if (j   ==      8       &&      k       ==      9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+
+}
