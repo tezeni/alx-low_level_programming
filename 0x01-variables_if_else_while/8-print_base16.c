b765cadbd522ab5784b2116f656adaf7cb6e7d4d
tezeni <teztig@gmail.com>
2023-04-21 17:02:32 -0700

    cm

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success):
+**/
+int main(void)
+{
+int i;
+int j;
+for (i  =       0;    i       <=      9;    i++)
+{
+putchar('0'     +       i);
+}
+for (j	=	0; j	<	6; j++)
+{
+putchar('97'	+	j);
+}
+putchar('\n');
+return  (0);
+}
