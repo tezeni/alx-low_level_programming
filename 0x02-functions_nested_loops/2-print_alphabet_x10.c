3a20fd8b434a5663e70b3a9398d1c1691639562e
tezeni <teztig@gmail.com>
2023-04-25 14:23:34 -0700

    nested

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,12 +3,13 @@
  *  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
  *   *
  *    * followed by anew line
-**/        
+**/      
 void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
 	i = 0;
+
 	while (i < 10)
 	{
 		ch = 'a';
