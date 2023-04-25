6e2def6007083220e23d5220d7df859d3ed16adf
tezeni <teztig@gmail.com>
2023-04-25 15:16:35 -0700

    nested

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,9 +10,9 @@ int print_last_digit(int n)
 
 	if (n	<	0)
 		n	=	-n;
-		a	=	n	%	10;
+	a	=	n	%	10;
         if (a	<	0)
 		a = -a;
-		_putchar(a + '0');
+	_putchar(a + '0');
 	return (a);            
 }
