11a31c6ef050918ebf5ab30bef4799834d51966b
tezeni <teztig@gmail.com>
2023-04-25 13:27:36 -0700

    nested

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
-/*
-** main - print _putchar
-** is written by mbah
-** Return: 0
-*/
+/**
+ *  * main - print _putchar
+ *   *
+ *    * Return: Always 0 (Success):
+**/
 int main(void)
 {
 _putchar('_');
