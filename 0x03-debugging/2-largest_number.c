20b001dc77fd0fa6c8358ba1c4ae73f97c2acab5
tezeni <teztig@gmail.com>
2023-05-02 00:28:35 -0700

    com

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,19 +8,19 @@
  * Return: largest number
 **/
 
-int largest_number(int	a,	int	b,	int	c)
+int	largest_number(int	a,	int	b,	int	c)
 {
 	int	largest;
 
-	if (a	>	b	&&	a	>	c)
+	if	(a	>	b	&&	a	>	c)
 	{
 		largest	=	a;
 	}
-	else if (a	>	b	&&	c	>	a)
+	else	if	(a	>	b	&&	c	>	a)
 	{
 		largest	=	c;
 	}
-	else if (b	>	c)
+	else	if	(b	>	c)
 	{
 			largest =	b;
 	}
@@ -28,5 +28,5 @@ int largest_number(int	a,	int	b,	int	c)
 	{
 		largest	=	c;
 	}
-	return (largest);
+	return	(largest);
 }
