61b8f2dfe2a9fc9d9896b61fd9174c50c7c37fb5
tezeni <teztig@gmail.com>
2023-05-02 02:17:55 -0700

    com

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,10 @@
 #ifndef main_h
-        #define main_h
-        #include <stdio.h>
-        int     largest_number(int      a,      int     b,      int     c);
-	int	positive_or_negative(int i);
-        #endif
+
+#define main_h
+
+#include <stdio.h>
+
+int     largest_number(int      a,      int     b,      int     c);
+void	positive_or_negative(int i);
+
+#endif
