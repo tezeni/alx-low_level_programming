e63ddc408b227f2b3e2cfc6d0970c832cd655d08
tezeni <teztig@gmail.com>
2023-05-04 13:56:08 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,18 +9,20 @@
 **/
 
 char *_strncpy(char *dest, char *src, int n)
+
 {
-	int i = 0;
-	int slen = 0;
 
-	while (src[i])
-	slen++;
+	int index = 0, src_len = 0;
 
-	for (i = 0; src[i] && i < n; i++)
-	dest[i] = src[i];
+	while (src[index++])
+	src_len++;
 
-	for (i = slen; i < n; i++)
-	dest[i] = '\0';
+	for (index = 0; src[index] && index < n; index++)
+	dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+	dest[index] = '\0';
 
 	return (dest);
+
 }
