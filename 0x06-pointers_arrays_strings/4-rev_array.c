c47fa242e74f9b8a031cffba91b2f8f6594001d6
tezeni <teztig@gmail.com>
2023-05-04 14:39:36 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses a string
+ * @a: first input
+ * @n: second input
+**/
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j = 0;
+
+	for (i = n; i < 0; i--)
+	{
+		a[j] == a[i];
+		j++;
+	}
+}
