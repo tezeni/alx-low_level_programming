62b1cff7410fb71d25e74512a70681593578ca98
tezeni <teztig@gmail.com>
2023-05-04 17:23:33 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,18 +9,19 @@
  */
 
 void print_number(int n)
+
 {
-	unsigned int i = n;
+
+	unsigned int num = n;
 
 	if (n < 0)
 	{
-		n *= -1;
-		i = n;
-		_putchar('-');
+	_putchar('-');
+	num = -num;
 	}
-	i /= 10;
-	if (i != 0)
-	print_number(i);
-	_putchar((unsigned int)num % 10 + '0');
+
+	if ((num / 10) > 0)
+	print_number(num / 10);
+	_putchar((num % 10) + '0');
 
 }
