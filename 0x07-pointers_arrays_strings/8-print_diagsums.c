f46cfb363d37014de9233fb9e27ca64a37c48097
tezeni <teztig@gmail.com>
2023-05-08 13:36:30 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - function to check a char
+ * @a: array input
+ * @size: array input
+ * Return: void
+**/
+void print_diagsums(int *a, int size)
+{
+	int i, j, a = 0, b = 0;
+
+	for (i = 0; i <= (size * size); i = i + size + 1)
+		a = a + a[i];
+
+	for (j = size - 1; j <= (size * size) - size; j = j + size - 1)
+	b = b + a[j];
+	printf("%d, %d\n", a, b);
+}
