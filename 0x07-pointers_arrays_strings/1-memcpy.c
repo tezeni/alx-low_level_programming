46846d27eb4687e936810a09a9a2fb5f20409a4d
tezeni <teztig@gmail.com>
2023-05-08 05:00:32 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy - Entry point
+ * @dest: pointed destination
+ * @src: constant byte
+ * @n: bytes
+ * Return: Always dest (Success)
+**/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+		
+		for (i = 0; i < n; i++)
+                dest[i] = src[i];
+        return (dest);
+}
