5c85e55f83262b2555451ba4e283b76a36e10675
tezeni <teztig@gmail.com>
2023-05-08 04:31:21 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * _memset - function to change the memory size
+ * @s: array to change memset
+ * @b: the caracter that replaces the mem
+ * @n: number of characters to be changed
+ * Return: the array modified s
+**/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	for (int i=0; i<n; i++)
+	{
+		char s[i] = b;
+	}
+	return (s);
+}
