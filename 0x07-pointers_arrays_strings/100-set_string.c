db2d63b68a18dc982363cfa8d584df8bb0daa23f
tezeni <teztig@gmail.com>
2023-05-08 13:49:36 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - function to check a char
+ * @s: array to be checked
+ * @to: the char to be assigned to
+ * Return: void
+**/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
