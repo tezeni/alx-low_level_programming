c1e6a935cadfdc9cfe1137b6088c4328d34239a8
tezeni <teztig@gmail.com>
2023-05-10 14:36:27 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function to calculate the power of given input
+ * @x: the input to do the power for
+ * @y: the input which is the power it self
+ * Return: integer
+**/
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return (x * _pow_recursion(x, y--));
+}
