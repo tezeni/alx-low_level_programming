b712901651e35ae4eba492a2149ee992670911a1
tezeni <teztig@gmail.com>
2023-05-12 12:55:49 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - fuction with arguments
+ * @argc: an intiger passed
+ * @argv: a pointer to the character passed
+ * Return: success
+**/
+int main(int argc, char *argv[])
+{
+	_putchar(argv[0]);
+	_putchar('\n');
+	exit(EXIT_SUCCESS);
+}
