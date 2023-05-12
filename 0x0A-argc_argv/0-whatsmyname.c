312f79b7d8628e2597c3baa9dfb45d1bcc34dc9b
tezeni <teztig@gmail.com>
2023-05-12 13:47:41 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+#include <unistd.h>
 
 /**
  * main - fuction with arguments
@@ -8,9 +9,26 @@
  * @argv: a pointer to the character passed
  * Return: success
 **/
+int _putchar(char c);
+
 int main (int argc, char *argv[])
 {
-	_putchar(*argv[0]);
-	_putchar('\n');
+	if (argc > 0)
+	{
+		_putchar(*argv[0]);
+		_putchar('\n');
+	}
 	return (0);
+}
+
+/**
+ *  * _putchar - writes the character c to stdout
+ *   * @c: The character to print
+ *    *
+ *     * Return: On success 1.
+ *      * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+                return (write(1, &c, 1));
 }
