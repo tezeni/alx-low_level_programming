68dcc1eafa22dda219b4d2667d17f2f8b147077d
tezeni <teztig@gmail.com>
2023-05-12 06:04:06 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,29 +8,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	int _strlen_recursion(char *s);
-	int len;
-
-	len = _strlen_recursion(s);
-
-	if (len < 0)
-		return;
-	_putchar(*(s + len));
-	len--;
-	_print_rev_recursion(s + 1);
-}
-
-/**
- * _strlen_recursion - function to count string length
- * @s: the string to be counted
- * Return: the length of the string
-**/
-
-int _strlen_recursion(char *s)
-{
-	if (*s == '\0')
+	if (*s)
 	{
-		return (0);
+		_print_rev_recursion(s + 1);
+		-putchar(*s);
 	}
-	return (1 + _strlen_recursion(s + 1));
 }
