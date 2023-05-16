ffdceb7324b1f60e23091fdc780156c418175483
tezeni <teztig@gmail.com>
2023-05-16 06:41:24 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - function to duplicate strings
+ * @str: string to be duplicated
+ * Return: return NULL or duplicated string
+**/
+
+char *_strdup(char *str)
+{
+	char *a;
+	unsigned int i, j;
+
+	i = strlen(str);
+
+	a = malloc((i + 1) * sizeof(char));
+
+	if (i != 0)
+	{
+		if (a == 0)
+			return (NULL);
+		for (j = 0; j < (i + 1); j++)
+		{
+			a[j] = str[j];
+		}
+		return (a);
+	}
+	else
+		return (NULL);
+}
