b3c9d11711012f76f40945c698c244899b4a02e6
tezeni <teztig@gmail.com>
2023-05-16 11:48:46 -0700

    2-str_concat.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - concatinate two strings
+ * @s1: the first string
+ * @s2: the second str
+ * Return: pointer to concatnated strings
+**/
+int **alloc_grid(int width, int height)
+{
+
