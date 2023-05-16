2a490e543ee490065f0b2752f3cc611ce4f9a69d
tezeni <teztig@gmail.com>
2023-05-16 11:02:40 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,16 +18,16 @@ char *str_concat(char *s1, char *s2)
 	i = 0;
 	a = malloc((strlen(s1) + strlen(s2) + 1) * sizeof(char));
 
-	if (s1 == NULL)
+	if (*s1 == NULL)
 	{
-		if (s2 == NULL)
+		if (*s2 == NULL)
 			a = 0;
 		else
 			a = s2;
 	}
-	if (s2 == NULL)
+	if (*s2 == NULL)
 	{
-		if (s1 == NULL)
+		if (*s1 == NULL)
 			a = 0;
 		else
 			a = s1;
