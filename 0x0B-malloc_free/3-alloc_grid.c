cbaf035de1d2ae77cf193c641614983fe58f0668
tezeni <teztig@gmail.com>
2023-05-16 23:30:32 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	a = (int**) malloc(height * sizeof(int *));
+	a = (int **) malloc(height * sizeof(int *));
 
 	if (a == NULL)
 	{
