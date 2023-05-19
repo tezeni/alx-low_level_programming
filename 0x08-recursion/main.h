95e5006523be9a937d7fd1ab896023545b372b00
tezeni <teztig@gmail.com>
2023-05-19 00:57:36 -0700

    6-is_prime_number.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,5 +12,7 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int find_sqrt(int num, int root);
+int is_divisible(int num, int div);
+int is_divisible(int num, int div);
 
 #endif
