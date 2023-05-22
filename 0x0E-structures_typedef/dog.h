a905eafcefdca8058edcdc6bb0b542bd7ce7d6ec
tezeni <teztig@gmail.com>
2023-05-22 13:10:09 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,4 +16,14 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_ d - Typedef for dog structure
+ */
+typedef struct dog dog_ d;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
