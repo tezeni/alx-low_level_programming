2bda76a535f308d10ec2b14098a8bd64366d759d
tezeni <teztig@gmail.com>
2023-05-25 09:40:46 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -44,48 +44,48 @@ char *_strcpy(char *dest, char *src)
 	return (dest);
 }
 /**
-  * new_dog - ...
-  * @name: ...
-  * @age: ...
-  * @owner: ...
+  * new_dog - struct for new dog
+  * @name: new dog name
+  * @age: new dog age
+  * @owner: new dog owner
   *
-  * Return: ...
+  * Return: new dog
   */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *my_dog;
+	dog_t *bla_dog;
 	int name_l = 0, own_l = 0;
 
 	if (name != NULL && owner != NULL)
 	{
 		name_l = _strlen(name) + 1;
 		own_l = _strlen(owner) + 1;
-		my_dog = malloc(sizeof(dog_t));
+		bla_dog = malloc(sizeof(dog_t));
 
-		if (my_dog == NULL)
+		if (bla_dog == NULL)
 			return (NULL);
 
-		my_dog->name = malloc(sizeof(char) * name_l);
+		bla_dog->name = malloc(sizeof(char) * name_l);
 
-		if (my_dog->name == NULL)
+		if (bla_dog->name == NULL)
 		{
-			free(max_dog);
+			free(bla_dog);
 			return (NULL);
 		}
 
-		my_dog->owner = malloc(sizeof(char) * own_l);
+		bla_dog->owner = malloc(sizeof(char) * own_l);
 
-		if (my_dog->owner == NULL)
+		if (bla_dog->owner == NULL)
 		{
-			free(my_dog->name);
-			free(my_dog);
+			free(bla_dog->name);
+			free(bla_dog);
 			return (NULL);
 		}
 
-		my_dog->name = _strcpy(my_dog->name, name);
-		my_dog->owner = _strcpy(my_dog->owner, owner);
-		my_dog->age = age;
+		bla_dog->name = _strcpy(bla_dog->name, name);
+		bla_dog->owner = _strcpy(bla_dog->owner, owner);
+		bla_dog->age = age;
 	}
 
-	return (my_dog);
+	return (bla_dog);
 }
