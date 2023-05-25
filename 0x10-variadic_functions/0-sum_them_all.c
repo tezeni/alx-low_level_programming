fc0d549b4f40f04d0888e1667bb390ad920ee27f
tezeni <teztig@gmail.com>
2023-05-25 12:45:01 -0700

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * sum_them_all - function to add arguments passed
+ * @n: argument passed to th fun
+ * Return: 0  or sum
+**/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	if (n != 0)
+	{
+		va_list a;
+		int i;
+		int sum;
+
+		sum = 0;
+
+		va_start (a, n);
+		for (i = 0; i < n, i++)
+		{
+			sum += va_arg(a, int);
+		}
+		return (sum);
+	}
+	return (0);
+}
