fe5aa4982a6abadf5a8581cb24e620ae4b56c60f
tezeni <teztig@gmail.com>
2023-06-01 12:35:04 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - function to count nodes
+ * @h: linked lists
+ * Return: nodes
+**/
+
+size_t print_list(const list_t *h)
+{
+        size_t c = 0;
+
+        while (h != NULL)
+        {
+                if (h->str == NULL)
+                        printf("[0] (nil)\n");
+                else
+                        printf("[%d] %s\n", h->len, h->str);
+
+                h = h->next;
+                c++;
+        }
+
+        return (c);
+}
