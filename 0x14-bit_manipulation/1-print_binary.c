136d130d5c855cf7d46559c2439dac26d6f299cd
tezeni <teztig@gmail.com>
2023-06-09 06:53:10 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,35 @@
+#include"main.h"
+
+/**
+  * _divide - ...
+  * @n: input number
+  * Return: noting
+  */
+void _divide(unsigned long int n)
+{
+	if (n < 1)
+		return;
+
+	_divide(n >> 1);
+
+	if (n & 1)
+		_putchar('1');
+	else
+		_putchar('0');
+}
+
+/**
+ * print_binary - Prints the binary representation of a number
+ * @n: number to be represented as binary
+ * Return: Nothing
+**/
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	_divide(n);
+}
+
