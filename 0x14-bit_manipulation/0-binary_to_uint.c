ed7b71497075386e5a605080310a2e7339780cdd
tezeni <teztig@gmail.com>
2023-06-09 06:37:03 -0700

    converts binary

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,40 @@
+#include"main.h"
+
+/**
+ * strnlen - function to return the length of a string
+ * @s: input string
+ * Return: String length
+**/
+int strnlen(const char *s)
+{
+	int l = 0;
+
+	while (s[l])
+		l++;
+	return (l);
+}
+
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int
+ * @b: The binary string to converts
+ * Return: positive number conerted from binary
+**/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int l = 0, c = 0, sum = 0;
+
+	if (b == NULL)
+		return (0);
+
+	l = strnlen(b);
+
+	while (l--)
+	{
+		if (b[l] != 48 && b[l] != 49)
+			return (0);
+		if (b[l] == 49)
+			s += 1 << c;
+		c++;
+	}
+	return (s);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN
 #define MAIN
 #include<stdio.h>
-
+#include <stdlib.h>
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
