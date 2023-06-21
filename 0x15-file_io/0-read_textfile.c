c6ec710b4028de86faa75bc5e03a346b2bc1d723
tezeni <teztig@gmail.com>
2023-06-21 06:15:34 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include"main.h"
+
+/**
+ * read_textfile - function to read text.
+ * @filename: file to read
+ * @letters: letters to find
+ * Return: file readed
+**/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, readd;
+	char *buf = malloc(sizeof(char *) * letters);
+
+	if (!buf)
+		return (0);
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY, 0600);
+	if (fd == -1)
+		return (0);
+
+	readd = read(fd, buff, letters);
+	write(STDOUT_FILENO, buf, readd);
+
+	free(buf);
+	close(fd);
+	return (reade);
+}
