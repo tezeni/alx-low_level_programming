90eaa594298b2b1ff7acc10138e132d60dcdcfff
Bettyabnet <Betelhemabnet1@gmail.com>
2023-10-30 02:02:03 -0700

    all

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
+ *
+ * Return: Upon failure - 0.
+ *         Otherwise - 1.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
